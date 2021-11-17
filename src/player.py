66becb133543618d8c1d093843dec40b7af81d7a
Tucker R. Twomey <tucker@the-twomeys.com>
2021-11-17 02:18:00 -0800

    Tighten up space around informational messages

--- src/player.py
+++ src/player.py
@@ -19,18 +19,14 @@ class Player:
     def showInventory(self):
         clear()
         print("You are currently carrying:")
-        print()
         for i in self.items:
             print(i.name)
         print()
-        input("Press enter to continue...")
     def attackMonster(self, mon):
         clear()
         print("You are attacking " + mon.name)
-        print()
         print("Your health is " + str(self.health) + ".")
         print(mon.name + "'s health is " + str(mon.health) + ".")
-        print()
         if self.health > mon.health:
             self.health -= mon.health
             print("You win. Your health is now " + str(self.health) + ".")
@@ -39,5 +35,4 @@ class Player:
             print("You lose.")
             self.alive = False
         print()
-        input("Press enter to continue...")
 
