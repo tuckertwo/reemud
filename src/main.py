33bd677d6a87d850fee27e877c50a9b6cbd9f07b
Tucker R. Twomey <tucker@the-twomeys.com>
2021-11-17 01:25:45 -0800

    Fix bug

--- src/main.py
+++ src/main.py
@@ -94,7 +94,7 @@ class CmdParseError(Exception):
 class Command():
     def __init__(self):
         if not hasattr(self, "args_hr"):
-            if len(self.args) >= 2:
+            if self.args is not None and len(self.args) >= 2:
                 self.args_hr = " ".join([str(x) for x in self.args[1:]])
             else:
                 self.args_hr = ""
