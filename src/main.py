802ed6238b7b85888a1a3f0eb7943fe8964c8876
Tucker R. Twomey <tucker@the-twomeys.com>
2021-11-17 01:05:13 -0800

    Add comments to explain parser

--- src/main.py
+++ src/main.py
@@ -106,26 +106,36 @@ class Command():
     def cmdparse(self, cmdstr):
         # Awful mutable data stuff ahead
         def parser(args_parsed, args_inp, args_to_fill):
+            # If out of stuff to parse
             if len(args_inp) == 0:
+                # Premature end?
                 if len(args_to_fill) == 0 or args_to_fill[0].optional or \
                     (args_to_fill[0].infinite and
                      args_to_fill[0].name in args_parsed):
                     return args_parsed
                 else:
                     raise CmdParseError("not enough arguments")
+            # Out of buckets and still more arguments exist?
             elif len(args_to_fill) == 0:
                 raise CmdParseError("too many arguments")
             else:
+                # Defer setting convenience variables until we are sure that
+                # the queues are not empty
                 tofill = args_to_fill[0]
                 toproc = args_inp[0]
-                if tofill is None:
+                if tofill is None: # Ignore this argument
                     return parser(args_parsed, args_inp[1:], args_to_fill[1:])
+                # Handle placeholder/nonsense arguments, used to make commands
+                # appear more English-like
+                # (*e.g.* `kill troll with axe` as opposed to `kill troll axe`)
                 elif tofill.nonsense:
                     if toproc == tofill.name:
                         return parser(args_parsed, args_inp[1:],
                                       args_to_fill[1:])
                     else:
                         return parser(args_parsed, args_inp, args_to_fill[1:])
+                # Infinite arguments are placed at the end of a string of
+                # args to capture the remainder of the command.
                 elif tofill.infinite:
                     if tofill.name in args_parsed:
                         args_parsed[tofill.name].append(toproc)
@@ -135,13 +145,16 @@ class Command():
                         args_parsed[tofill.name] = [toproc]
                         return parser(args_parsed, args_inp[1:],
                                       args_to_fill[1:])
+                # Normal (non-infinite, non-nonsense) arguments
                 else:
-                        args_parsed[tofill.name] = toproc
-                        return parser(args_parsed, args_inp[1:],
-                                      args_to_fill[1:])
+                    args_parsed[tofill.name] = toproc
+                    return parser(args_parsed, args_inp[1:],
+                                    args_to_fill[1:])
 
+        # Bail out if argument processing is unneeded
         if not self.args:
             return None
+        # Otherwise, start the process
         else:
             return parser({}, good_split_spc(cmdstr), self.args)
 
