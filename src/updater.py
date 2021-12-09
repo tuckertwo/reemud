e038d62ef4ea1017d6707d7c5525b08f8a7ee749
atomicmoloch <89928817+atomicmoloch@users.noreply.github.com>
2021-12-09 00:12:45 -0800

    s

--- src/updater.py
+++ src/updater.py
@@ -17,6 +17,12 @@ def allocateLoot():
     for i in range(5):
         loot.append(item.Weapon("Sword", "A nondescript metal sword.", 6, 3))
         loot.append(item.HealingPotion(15))
+        loot.append(item.Poison(15)
+        loot.append(item.Regeneration(15))
+        loot.append(item.Water(15))
+        loot.append(item.DamageScroll(10))
+        loot.append(item.PoisonScroll(3))
+        loot.append(item.Fireball(8))
     loot.append(item.Weapon("Epic sword", "This sword is so cool, its surprising its wielded by an ordinary skeleton", 100, 3))
     while len(loot) > 0:
         x = random.randrange(len(loot))
